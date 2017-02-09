854e70d20cd892c4d9a630c5fe8a4e4e059f5253
lummish <lummish@gmail.com>
2017-02-09 10:53:06 -0800

    added tests and fixed line termination

--- test/test.js
+++ test/test.js
@@ -15,3 +15,40 @@ describe('Arithmetic', function() {
     });
   });
 });
+
+describe('Functions', function() {
+    describe('fun a(): \n indent \n dedent', function() {
+        it('should succeed on empty block', function() {
+            let match = gram.match('fun a(): \n indent dedent');
+            assert.ok(match.succeeded());
+        });
+    });
+
+    describe('fun a(): \n indent ret 3 + 2 dedent', function() {
+        it('should succeed on normal block', function() {
+            let match = gram.match('fun a(): \n indent ret 3 + 2 \n dedent');
+            assert.ok(match.succeeded());
+        });
+    });
+
+    describe('fun a(x y): \n indent ret 3 + x dedent', function() {
+        it('should succeed on parameters', function() {
+            let match = gram.match('fun a(x y): \n indent ret 3 + x \n dedent');
+            assert.ok(match.succeeded());
+        });
+    });
+
+    describe('(x y): \n indent ret 3 + x - y dedent', function() {
+        it('should succeed on function literal', function() {
+            let match = gram.match('(x y): \n indent ret 3 + x \n dedent');
+            assert.ok(match.succeeded());
+        });
+    });
+
+    describe('(x y): \n indent z = y \n ret 3 + x - y \n dedent', function() {
+        it('should succeed on function literal (multiple expressions)', function() {
+            let match = gram.match('(x y): \n indent z = y \n ret 3 + x \n dedent');
+            assert.ok(match.succeeded());
+        });
+    });
+});
