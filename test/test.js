3893c39403f206d497b0d25aff60b71f36798375
Tyler C <Teecolz@users.noreply.github.com>
2017-02-09 09:14:49 -0800

    added arithmetic tests

--- test/test.js
+++ test/test.js
@@ -9,8 +9,20 @@ var assert = require('assert');
 
 describe('Arithmetic', function() {
   describe('3 + 2', function() {
-    it('should succeed', function() {
+    it('should be equivilent to result', function() {
       let match = gram.match('2 + 3');
+      assert.ok(match.succeeded());
+    });
+  });
+  describe('8 * 8', function() {
+    it('should be equivilent to result', function() {
+      let match = gram.match('2 ** 6');
+      assert.ok(match.succeeded());
+    });
+  });
+  describe('8 % 2', function() {
+    it('should be equivilent to result', function() {
+      let match = gram.match('100 % 2');
       assert.ok(match.succeeded());
     });
   });
