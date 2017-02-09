fed48d3e01514b1d8ce311b4e760c70ede995def
lummish <lummish@gmail.com>
2017-02-09 01:28:33 -0800

    added example test in mocha

--- test/test.js
+++ test/test.js
@@ -5,18 +5,13 @@ const ohm = require('ohm-js');
 
 const gram = ohm.grammar(bool);
 
-let match = gram.match('2 + 3');
-
-if (match.succeeded()) {
-    console.log('Simple Arithmetic Match');
-}
-
 var assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+describe('Arithmetic', function() {
+  describe('3 + 2', function() {
+    it('should succeed', function() {
+      let match = gram.match('2 + 3');
+      assert.ok(match.succeeded());
     });
   });
 });
