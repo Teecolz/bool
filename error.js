0a2a4f8895744b537ae457e91e4dcf7ec1199c73
lummish <lummish@gmail.com>
2017-02-28 17:03:54 -0800

    added error tracking

---
+++ error.js
@@ -0,0 +1,19 @@
+const error = (message, location) => {
+  let msg = message;
+
+  if (location && location.line) {
+    msg += ` at line ${location.line}`;
+    if (location.col) {
+      msg += `, column ${location.col}`;
+    }
+  }
+  if (!error.quiet) {
+    console.log(`Error: ${msg}`);
+  }
+  error.count += 1;
+};
+
+error.quiet = false;
+error.count = 0;
+
+module.exports = error;
