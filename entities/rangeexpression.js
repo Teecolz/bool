017a2610d9eeafc9edc7861136da2342f18ba597
lummish <lummish@gmail.com>
2017-04-22 18:43:53 -0700

    amended rangeexp constructor

--- entities/rangeexpression.js
+++ entities/rangeexpression.js
@@ -1,8 +1,20 @@
+const IntegerLiteral = require('./intliteral.js');
+
 class RangeExpression {
   constructor(start, end, step) {
-    this.start = start;
-    this.end = end;
-    this.step = step;
+    if (end.length === 0) {
+      this.start = new IntegerLiteral('0');
+      this.end = start;
+      this.step = new IntegerLiteral('1');
+    } else if (step[0].length === 0) {
+      this.start = start;
+      this.end = end[0];
+      this.step = new IntegerLiteral('1');
+    } else {
+      this.start = start;
+      this.end = end[0];
+      this.step = step[0];
+    }
   }
   toString() {
     return `(Range ${this.start}, ${this.end}, ${this.step})`.replace(/(, )*\)$/, ')');
