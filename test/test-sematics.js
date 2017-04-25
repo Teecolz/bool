5dc5449eb5108e62507aeb63b989bb92670e865f
lummish <lummish@gmail.com>
2017-04-25 13:52:42 -0700

    cleaned up some linter errors

--- test/test-sematics.js
+++ test/test-sematics.js
@@ -1,14 +1,7 @@
 /* eslint-env node, mocha */
 const fs = require('fs');
-const ohm = require('ohm-js');
-const assert = require('assert');
 const parse = require('../parser.js');
 
-const bool = fs.readFileSync('bool.ohm');
-const gram = ohm.grammar(bool);
-
 const testDir = 'test/data/semantic-errors';
-// const p2 = parse(fs.readFileSync(`${testDir}/newIndent.bool`, { encoding: 'UTF-8' }));
 const program = parse(fs.readFileSync(`${testDir}/semantic-errors.bool`, { encoding: 'UTF-8' }));
-
 program.analyze();
