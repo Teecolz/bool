11e772a632440b0168b4afd8c876e32eacf4c63a
Jeremy <jhitchco@lion.lmu.edu>
2017-04-29 18:47:13 -0700

    added more tests for Strength Reduction

--- test/data/optimizer/output/integer-binexps.js
+++ test/data/optimizer/output/integer-binexps.js
@@ -12,3 +12,18 @@ x_2 = x_2;
 x_2 = x_2;
 x_2 = 1;
 x_2 = 0;
+let y_3 = 3;
+y_3 = 0;
+y_3 = 1;
+y_3 = -y_3;
+y_3 = 0;
+y_3 = 1;
+if (false) {
+  true;
+}
+if (false) {
+  true;
+}
+if (true) {
+  true;
+}
